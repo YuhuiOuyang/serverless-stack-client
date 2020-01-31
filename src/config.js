b3a65cd43090404836561b725173fdd1bd547e61
YuhuiOuyang <YuhuiOuyang@gmail.com>
2020-01-30 20:15:51 -0500

    Setting up react app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-1",
+        BUCKET: "notes-app-uploads-ouyangy"
+    },
+    apiGateway: {
+        REGION: "us-east-1",
+        URL: "https://3zlf1huv9g.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-1",
+        USER_POOL_ID: "us-east-1_4S7VxU7Wx",
+        APP_CLIENT_ID: "3aii2t3cdp60nt7ccmscifauk5",
+        IDENTITY_POOL_ID: "us-east-1:73e06c31-7904-4701-8d65-4735182e4b48"
+    }
+};
